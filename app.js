3de4200e1f75ea77346c0ff2791aac84520511f1
Atif Rehman <atifrehman401@gmail.com>
2024-03-03 18:45:41 +0000

    FUNCTIONS

---
+++ app.js
@@ -0,0 +1,43 @@
+// function function(p1,p2) {
+//     return p1*p2;
+// }
+//  var =p1
+//  var =p2
+//  console.log("p1,p2")
+// var a =1;
+// var b =2;
+// var c =3;
+
+// console.log ("sum of a and b", a+b+c);
+
+// console.log ("sum of a and b", a+b+c);
+// console.log ("sum of a and b", a+b+c);
+// console.log ("sum of a and b", a+b+c);
+
+// Function is called, the return value will end up in x
+
+
+// function Hello() {
+//     document.write("Hello Everybody")
+// }
+// Hello();
+
+// function karachi(params) {
+//     document.write("karachi is good")
+// }
+// karachi();
+
+// function Atif(params) {
+//     document.write("my name is Atif")
+// }
+// Atif();
+
+function Hello(fname,lname) {
+    document.write("Hello"+fname+ +lname);
+}
+Hello("fname","lname");
+
+function sum(a,b) {
+    document.write(a+b);
+}
+sum(20,50);
